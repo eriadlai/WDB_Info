e1c4f6760937d0bcd69876b55775791faf77f56a
AdlaiCG <eri_acg@hotmail.com>
2021-07-08 11:10:13 -0700

    Base CRUD AspNet

---
+++ CRUD/Controllers/UsuariosController.cs
@@ -0,0 +1,106 @@
+﻿using CRUD.Data;
+using CRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsuariosController(ApplicationDbContext context) {
+            _context = context;
+        }
+        //Https Get Index
+        public IActionResult Index()
+        {
+            IEnumerable<Usuario> listUsuario = _context.Usuario;
+            return View(listUsuario);
+        }
+        //Https Post Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Usuario usuario) {
+            if (ModelState.IsValid)
+            {
+                _context.Usuario.Add(usuario);
+                _context.SaveChanges();
+                TempData["message"] = "El usuario ha sido creado";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        //Https Get Create
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //Https Get Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var usuario = _context.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return View(usuario);
+        }
+        //Https Post Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Usuario.Update(usuario);
+                _context.SaveChanges();
+                TempData["message"] = "El usuario se ha actualizado";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //Https Post Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteUsuario(int? id)
+        {
+            var usuario = _context.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+           
+                _context.Usuario.Remove(usuario);
+                _context.SaveChanges();
+            TempData["message"] = "El usuario se ha Eliminado";
+         
+            return RedirectToAction("Index");
+        }
+        //Https Get Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var usuario = _context.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return View(usuario);
+        }
+    }
+}
---
+++ CRUD/Migrations/20210707193252_Initial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUD.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    password = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    nacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    apellido = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    profesion = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
