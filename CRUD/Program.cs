fdf1db52ac8eb70560b8426dd2c867f003a48128
AdlaiCG <eri_acg@hotmail.com>
2021-07-26 10:07:53 -0700

    PruebaCRUDFactura

--- CRUD/Program.cs
+++ CRUD/Program.cs
@@ -4,8 +4,11 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace CRUD
 {
@@ -14,6 +17,7 @@ namespace CRUD
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+           
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
